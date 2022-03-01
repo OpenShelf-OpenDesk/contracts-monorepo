e80c13fe3b3e309b376cc6dff0d30076d9030ee5
Raghav Goyal <12301raghavgoyal@gmail.com>
2022-03-01 20:04:55 +0530

    deploy updated contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-// require("@openzeppelin/hardhat-upgrades");
+require("@openzeppelin/hardhat-upgrades");
 require("dotenv").config({ path: __dirname + "/.env" });
 
 // You need to export an object to set up your config
@@ -8,42 +8,42 @@ require("dotenv").config({ path: __dirname + "/.env" });
 const contractVersion = 3;
 
 const config = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.7",
-      },
-    ],
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 2000,
-      },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+        ],
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 2000,
+            },
+        },
     },
-  },
-  paths: {
-    sources: `./contracts/v${contractVersion}`,
-  },
-  networks: {
-    ganache: {
-      url: "HTTP://127.0.0.1:7545",
-      accounts: {
-        mnemonic:
-          "tag palace accident hidden delay escape involve fetch mushroom corn settle doctor",
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 10,
-      },
+    paths: {
+        sources: `./contracts/v${contractVersion}`,
+    },
+    networks: {
+        ganache: {
+            url: "HTTP://127.0.0.1:7545",
+            accounts: {
+                mnemonic:
+                    "tag palace accident hidden delay escape involve fetch mushroom corn settle doctor",
+                path: "m/44'/60'/0'/0",
+                initialIndex: 0,
+                count: 10,
+            },
 
-      chainId: 1337,
+            chainId: 1337,
+        },
+        polytest: {
+            url: process.env.MUMBAI_ALCHEMY_URL || "",
+            gas: 2100000,
+            gasPrice: 8000000000,
+            accounts: [`0x${process.env.MUMBAI_DEPLOYER_PRIV_KEY}`],
+        },
     },
-    polytest: {
-      url: process.env.MUMBAI_ALCHEMY_URL || "",
-      gas: 2100000,
-      gasPrice: 8000000000,
-      accounts: [`0x${process.env.MUMBAI_DEPLOYER_PRIV_KEY}`],
-    },
-  },
 };
 
 export default config;
